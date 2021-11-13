c4409ffbb73a2fe8e650ecc1c949d49dc763ed00
carmodysan <carmodysan@outlook.com>
2021-11-13 12:51:29 +0100

    Ajout des composants Axios et Dotenv Ajout d'un test Axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import axios from 'axios';
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL_API
+console.log('base url ! ' + process.env.VUE_APP_BASE_URL_API)
 
 new Vue({
   vuetify,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about page</h1>
+    <h3>{{ test }}</h3>
+	</div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+	data: () => ({
+		test: null,
+	}),
+
+	methods: {
+		async init() {
+			await axios
+				.get("/operations")
+				.then((response) => {
+					this.test = response.data["hydra:member"];
+				})
+				.finally(() => {});
+		},
+	},
+
+	created() {
+		this.init();
+	},
+};
+</script>
