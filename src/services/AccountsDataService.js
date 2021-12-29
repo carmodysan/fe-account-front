040c428dcc96c7e203044ed3f50a938dab546251
carmodysan <carmodysan@outlook.com>
2021-12-29 16:40:58 +0100

    Création d'un compte (WIP)

--- src/services/AccountsDataService.js
+++ src/services/AccountsDataService.js
@@ -1,76 +1,76 @@
 import axios from './FEApiService';
 
 class AccountsDataService {
-    /**
-     * Récupère tous les comptes d'un utilisateurs.
-     * 
-     * @param String authorId Identifiant de l'utilisateur
-     * @returns la liste des comptes.
-     */
+	/**
+	 * Récupère tous les comptes d'un utilisateurs.
+	 *
+	 * @param String authorId Identifiant de l'utilisateur
+	 * @returns la liste des comptes.
+	 */
 	getAll(authorId) {
-        const currentAccounts = this.getAllCurrentAccounts(authorId);
-        const savingsAccounts = this.getAllSavingsAccounts(authorId);
+		const currentAccounts = this.getAllCurrentAccounts(authorId);
+		const savingsAccounts = this.getAllSavingsAccounts(authorId);
 
-        return Promise.all([currentAccounts, savingsAccounts]);
+		return Promise.all([currentAccounts, savingsAccounts]);
 	}
-    
-    /**
-     * Récupère tous les comptes courants d'un utilisateur.
-     * 
-     * @param String authorId  Identifiant de l'utilisateur
-     * @returns la liste des comptes courants
-     */
-    getAllCurrentAccounts(authorId) {
-        return axios.get(`/current_accounts?authorId=${authorId}&order[establishment]`);
-    }
 
-    /**
-     * Récupère tous les comptes d'épargne d'un utilisateur.
-     * 
-     * @param String authorId  Identifiant de l'utilisateur
-     * @returns la liste des comptes d'épargne
-     */
-     getAllSavingsAccounts(authorId) {
-        return axios.get(`/savings_accounts?authorId=${authorId}&order[establishment]`);
-    }
+	/**
+	 * Récupère tous les comptes courants d'un utilisateur.
+	 *
+	 * @param String authorId  Identifiant de l'utilisateur
+	 * @returns la liste des comptes courants
+	 */
+	getAllCurrentAccounts(authorId) {
+		return axios.get(`/current_accounts?authorId=${authorId}&order[establishment]`);
+	}
 
-    /**
-     * Récupère un compte particulier.
-     * 
-     * @param String id Identifiant du compte
-     * @returns un compte.
-     */
+	/**
+	 * Récupère tous les comptes d'épargne d'un utilisateur.
+	 *
+	 * @param String authorId  Identifiant de l'utilisateur
+	 * @returns la liste des comptes d'épargne
+	 */
+	getAllSavingsAccounts(authorId) {
+		return axios.get(`/savings_accounts?authorId=${authorId}&order[establishment]`);
+	}
+
+	/**
+	 * Récupère un compte particulier.
+	 *
+	 * @param String id Identifiant du compte
+	 * @returns un compte.
+	 */
 	get(id) {
 		return axios.get(`/accounts/${id}`);
 	}
 
     /**
-     * Créé un compte
+     * Créé un compte courant.
      * 
-     * @param Account data 
-     * @returns Response
+     * @param {*} data les données du compte courant à créer
+     * @returns Response La response d'axios pour le POST
      */
-	create(data) {
-		return axios.post('/accounts', data);
-	}
+    createCurrentAccount(data) {
+        return axios.post(`/current_accounts`, data);
+    }
 
-    /**
-     * Met à jour un compte.
-     * 
-     * @param String id Identifiant du compte
-     * @param Account data 
-     * @returns Response
-     */
+	/**
+	 * Met à jour un compte.
+	 *
+	 * @param String id Identifiant du compte
+	 * @param Account data
+	 * @returns Response
+	 */
 	update(id, data) {
 		return axios.put(`/accounts/${id}`, data);
 	}
 
-    /**
-     * Supprime un compte.
-     * 
-     * @param String id Identifiant du compte
-     * @returns Response
-     */
+	/**
+	 * Supprime un compte.
+	 *
+	 * @param String id Identifiant du compte
+	 * @returns Response
+	 */
 	delete(id) {
 		return axios.delete(`/accounts/${id}`);
 	}
--- src/store/accounts.js
+++ src/store/accounts.js
@@ -1,4 +1,5 @@
 import AccountsDataService from '../services/AccountsDataService';
+import store from './index';
 
 export default {
 	namespaced: true,
@@ -16,7 +17,7 @@ export default {
 
 		/**
 		 * Boolean pour savoir si les comptes sont déjà récupérés.
-         * Utile pour éviter de faire des appels inutiles à l'API quand il y a des changements de pages (par exemple).
+		 * Utile pour éviter de faire des appels inutiles à l'API quand il y a des changements de pages (par exemple).
 		 */
 		accountsRetrieved: false,
 	},
@@ -42,15 +43,15 @@ export default {
 			return state.isAccountsRetrieving;
 		},
 
-        /**
-         * Retourne l'objet indiquant si les comptes sont déjà récupérés.
-         * 
-         * @param {Object} state 
-         * @returns l'objet accountsRetrieved
-         */
-        getAccountsRetrieved(state) {
-            return state.accountsRetrieved;
-        }
+		/**
+		 * Retourne l'objet indiquant si les comptes sont déjà récupérés.
+		 *
+		 * @param {Object} state
+		 * @returns l'objet accountsRetrieved
+		 */
+		getAccountsRetrieved(state) {
+			return state.accountsRetrieved;
+		},
 	},
 
 	mutations: {
@@ -64,14 +65,14 @@ export default {
 			state.accounts.push.apply(state.accounts, accounts);
 		},
 
-        /**
-         * Vide la liste de comptes.
-         * 
-         * @param {Object} state 
-         */
-        emptyAccounts(state) {
-            state.accounts = [];
-        },
+		/**
+		 * Vide la liste de comptes.
+		 *
+		 * @param {Object} state
+		 */
+		emptyAccounts(state) {
+			state.accounts = [];
+		},
 
 		/**
 		 * Définit le boolean si les comptes sont en cours de récupération.
@@ -83,15 +84,15 @@ export default {
 			state.isAccountsRetrieving = isAccountsRetrieving;
 		},
 
-        /**
-         * Définit le boolean indiquant si les comptes sont récupérés.
-         * 
-         * @param {Object} state 
-         * @param {Boolean} accountsRetrieved 
-         */
-        setAccountsRetrieved(state, accountsRetrieved) {
-            state.accountsRetrieved = accountsRetrieved;
-        }
+		/**
+		 * Définit le boolean indiquant si les comptes sont récupérés.
+		 *
+		 * @param {Object} state
+		 * @param {Boolean} accountsRetrieved
+		 */
+		setAccountsRetrieved(state, accountsRetrieved) {
+			state.accountsRetrieved = accountsRetrieved;
+		},
 	},
 
 	actions: {
@@ -105,7 +106,7 @@ export default {
 
 			// Seulement si les comptes n'ont pas déjà été récupérés
 			if (!state.accountsRetrieved) {
-                commit('emptyAccounts'); // On vide la liste des comptes au cas où elle ne serait pas vide.
+				commit('emptyAccounts'); // On vide la liste des comptes au cas où elle ne serait pas vide.
 				AccountsDataService.getAll(user)
 					.then((responses) => {
 						// On récupère toutes les réponses et on les ajoute dans le tableau des comptes
@@ -114,25 +115,66 @@ export default {
 						}
 					})
 					.catch((e) => {
+						commit('setIsAccountsRetrieving', false); // Les comptes ne sont plus en cours de récupération
 						throw e;
 					})
 					.finally(() => {
 						commit('setIsAccountsRetrieving', false); // Les comptes ne sont plus en cours de récupération
-                        commit('setAccountsRetrieved', true); // Les comptes ont été récupérés
+						commit('setAccountsRetrieved', true); // Les comptes ont été récupérés
 					});
 			} else {
-                commit('setIsAccountsRetrieving', false); // Les comptes ne sont plus en cours de récupération
-            }
+				commit('setIsAccountsRetrieving', false); // Les comptes ne sont plus en cours de récupération
+			}
 		},
 
-        /**
-         * Permet de forcer le rafraichissement dans le store de la liste des comptes disponibles avec le getter 'getAccounts'.
-         * 
-         * @param {*} user Id de l'utilisateur
-         */
-		refreshAccounts({dispatch, commit}, user) {
-            commit('setAccountsRetrieved', false); // On indique que les comptes sont à rafraichir en flaggant à false.
-            dispatch('retrieveAccounts', user); // On lance la récupération.
-        },
+		/**
+		 * Permet de forcer le rafraichissement dans le store de la liste des comptes disponibles avec le getter 'getAccounts'.
+		 *
+		 * @param {*} user Id de l'utilisateur
+		 */
+		refreshAccounts({ dispatch, commit }, user) {
+			commit('setAccountsRetrieved', false); // On indique que les comptes sont à rafraichir en flaggant à false.
+			dispatch('retrieveAccounts', user); // On lance la récupération.
+		},
+
+		/**
+		 * ==============> Partie Création des comptes <==============
+		 */
+		createAccount({ dispatch }, account) {
+			store.commit('setLoading', true); // On déclenche l'affichage du loader.
+
+			const categoryAccount = account.category; // On récupère la catégorie du compte
+
+			delete account.category; // On supprime la catégorie des données à envoyer
+			account.balance = '0'; // On ajoute le solde dans les données à envoyer
+
+			switch (categoryAccount) {
+				case 'Current':
+					dispatch('createCurrentAccount', account);
+                    break;
+				case 'Savings':
+					break;
+
+				default:
+                    store.commit('setLoading', false); // On arrête le loader
+					throw 'Error: category does not exist !';
+			}
+
+            store.commit('setLoading', false); // Au cas où on ne serait passé dans aucune des instructions ci-dessus.
+		},
+
+		createCurrentAccount({ dispatch }, account) {
+			account.upcomingBalance = '0'; // On ajoute le solde à venir dans les données à envoyer
+
+			AccountsDataService.createCurrentAccount(account)
+				.catch((e) => {
+					store.commit('setLoading', false); // On arrête le loader
+					throw e;
+				})
+				.finally(() => {
+                    store.commit('setLoading', false); // On arrête le loader
+                    dispatch('refreshAccounts', account.authorId);
+				});
+		},
 	},
 };
