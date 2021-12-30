37567fdf1d151aec9f75996be9ae7295abb3d175
carmodysan <carmodysan@outlook.com>
2021-12-30 22:29:46 +0100

    Ajout de règle de validation

--- src/config/formAccountHelper.js
+++ src/config/formAccountHelper.js
@@ -4,6 +4,7 @@ export default {
         accountName: [(v) => !!v || `Le nom du compte est obligatoire`],
         category: [(v) => !!v || `La catégorie du compte est obligatoire`],
         establishment: [(v) => !!v || `L'établissement' est obligatoire`],
+        year: [(v) => !!v || `L'année' est obligatoire`],
     },
 
     // Partie liste des items des v-select dans les formulaires
